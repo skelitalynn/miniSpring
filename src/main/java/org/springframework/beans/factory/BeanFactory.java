5691b2e5333ce6a38c076dcc26829fe870a197d2
skelitalynn <skelitalynn@gmail.com>
2025-11-20 21:12:53 +0800

    simple-bean-container

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -1,4 +1,8 @@
 package org.springframework.beans.factory;
 
+import org.springframework.beans.BeansException;
 
+public interface BeanFactory {
+
+    Object getBean(String name) throws BeansException;
 }
